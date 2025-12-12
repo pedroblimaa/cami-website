8d3b4b4374947fcd93c32edaa900ec50cc00af44
Pedro <pedroblimaa@gmail.com>
2025-12-11 21:48:33 -0300

    Add animations to Header and Hero page

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -6,13 +6,15 @@ export default function Hero() {
       <div>
         <h2 className='font-semibold text-gray-500 text-xl'>Dra. Camila Santos Castilho</h2>
         <h1 className='text-4xl font-lora text-brand font-semibold'>Médica generalista</h1>
-        <button
-          className='bg-brand px-8 py-3 rounded-2xl text-gray-50 mt-5 hover:bg-brand-light
-            hover:-translate-y-1.5 hover:shadow-md hover:shadow-gray-400 hover:text-white hover:scale-[1.01]
-            transition-all duration-400 cursor-pointer active:scale-90 active:shadow-sm'
-        >
-          Agende sua consulta
-        </button>
+        <a href='https://paciente.amorsaude.com.br/amorsaude' target='_blank' rel='noopener noreferrer'>
+          <button
+            className='bg-brand px-8 py-3 rounded-2xl text-gray-50 mt-5 transition-all duration-300 ease-in-out 
+            transform hover:-translate-y-1 hover:shadow-lg hover:bg-brand-light active:scale-95 active:shadow-md
+            cursor-pointer'
+          >
+            Agende sua consulta
+          </button>
+        </a>
       </div>
       <div className='h-full flex items-end'>
         <Image src='/images/img-transparent.png' alt='Camila' height={400} width={400} className='-mb-10' />
--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -1,25 +1,24 @@
 import Image from 'next/image'
+import HeaderMenuItem from './HeaderMenuItem'
 
 export default function Header() {
   return (
     <header className='flex items-center justify-between h-16 w-full max-w-7xl px-10'>
-      <div className='flex gap-5 items-center'>
-        <Image src={'/icons/logo.svg'} alt='Logo principal' width={45} height={45} />
-        <span>Dra. Camila Santos Castilho</span>
+      <div className='flex gap-3 items-center group cursor-pointer'>
+        <Image
+          src={'/icons/logo.svg'}
+          alt='Logo principal'
+          width={45}
+          height={45}
+          className='group-hover:rotate-360 duration-500 ease-in-out'
+        />
+        <span className='group-hover:translate-x-1 group-hover:text-brand duration-500 ease-in-out '>Dra. Camila Santos Castilho</span>
       </div>
       <ul className='flex gap-10'>
-        <li>
-          <button>Início</button>
-        </li>
-        <li>
-          <button>Sobre Mim</button>
-        </li>
-        <li>
-          <button>Localização</button>
-        </li>
-        <li>
-          <button>Contato</button>
-        </li>
+        <HeaderMenuItem text='Início' />
+        <HeaderMenuItem text='Sobre Mim' />
+        <HeaderMenuItem text='Localização' />
+        <HeaderMenuItem text='Contato' />
       </ul>
     </header>
   )
