3c1e43d19982c87dc4e16109244ac0912fe91987
Pedro <pedroblimaa@gmail.com>
2025-12-11 21:55:32 -0300

    Improve Hero button animation

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -8,11 +8,15 @@ export default function Hero() {
         <h1 className='text-4xl font-lora text-brand font-semibold'>Médica generalista</h1>
         <a href='https://paciente.amorsaude.com.br/amorsaude' target='_blank' rel='noopener noreferrer'>
           <button
-            className='bg-brand px-8 py-3 rounded-2xl text-gray-50 mt-5 transition-all duration-300 ease-in-out 
+            className='group relative bg-brand px-8 py-3 rounded-2xl text-gray-50 mt-5 transition-all duration-300 ease-in-out 
             transform hover:-translate-y-1 hover:shadow-lg hover:bg-brand-light active:scale-95 active:shadow-md
-            cursor-pointer'
+            cursor-pointer overflow-hidden'
           >
-            Agende sua consulta
+            <span
+              className='absolute top-0 -left-4 w-1/2 h-full bg-white opacity-10 transform -skew-x-12 -translate-x-full
+                group-hover:translate-x-[400%] transition-transform duration-700 ease-in-out'
+            ></span> 
+            <span className='relative'>Agende sua consulta</span>
           </button>
         </a>
       </div>
